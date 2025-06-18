3a17e1f7edb36294ab32546007dae379325bd7df
Greenfenix414 <greenfenix414@gmail.com>
2025-06-18 08:45:45 -0500

    added .self everywhere

--- main.py
+++ main.py
@@ -12,63 +12,67 @@ class game:
         self.sound=pygame.mixer.music.load("data/donuts.wav")
         pygame.mixer.music.play(-1)
         self.delta=0.1
-        speed =140
-        self.current_speed =speed
+        self.speed =140
+        self.current_speed =self.speed
         self.x=0
         self.y=0
         self.move_keys_down=0
         self.move_right = False
         self.move_down = False
         self.move_left = False
-        self.move_up = False           
+        self.move_up = False
+        self.running=True     
+              
             
     def run(self):
 
-        while running:
+        while self.running:
             self.screen.fill("white")
-            self.screen.blit(self.character_img,(x,y))
-            if move_keys_down>1:
-                current_speed=self.speed/2
+            self.screen.blit(self.character_img,(self.x,self.y))
+            if self.move_keys_down>1:
+                self.current_speed=self.speed/2
             else:
-                current_speed=self.speed   
-            if move_right:
-                x+=current_speed*delta
-            if move_down:
-                y+=current_speed*delta
-            if move_left:
-                x-=current_speed*delta
-            if move_up:
-                y-=current_speed*delta           
+                self.current_speed=self.speed   
+            if self.move_right:
+                self.x+=self.current_speed*delta
+            if self.move_down:
+                self.y+=self.current_speed*delta
+            if self.move_left:
+                self.x-=self.current_speed*delta
+            if self.move_up:
+                self.y-=self.current_speed*delta           
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
-                    running=False
+                    self.running=False
                 if event.type==pygame.KEYDOWN:
                     if event.key==pygame.K_d:
-                        move_keys_down+=1
-                        move_right=True
+                        self.move_keys_down+=1
+                        self.move_right=True
                     if event.key==pygame.K_s:
-                        move_keys_down+=1
-                        move_down=True     
+                        self.move_keys_down+=1
+                        self.move_down=True     
                     if event.key==pygame.K_a:
-                        move_keys_down+=1
-                        move_left=True
+                        self.move_keys_down+=1
+                        self.move_left=True
                     if event.key==pygame.K_w:
-                        move_keys_down+=1
-                        move_up=True                         
+                        self.move_keys_down+=1
+                        self.move_up=True                         
                 if event.type==pygame.KEYUP:
                     if event.key==pygame.K_d:
-                        move_keys_down-=1
-                        move_right=False
+                        self.move_keys_down-=1
+                        self.move_right=False
                     if event.key==pygame.K_s:
-                        move_keys_down-=1
-                        move_down=False     
+                        self.move_keys_down-=1
+                        self.move_down=False     
                     if event.key==pygame.K_a:
-                        move_keys_down-=1
-                        move_left=False
+                        self.move_keys_down-=1
+                        self.move_left=False
                     if event.key==pygame.K_w:
-                        move_keys_down-=1
-                        move_up=False                    
+                        self.move_keys_down-=1
+                        self.move_up=False                    
             pygame.display.flip()
             delta = self.clock.tick(60)/1000
             delta=max(0.001, min(0.1,delta))
         pygame.quit()
+        
+game().run()        
