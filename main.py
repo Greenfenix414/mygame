70ab1de32e8a092c7966a019b489a85544953c06
Greenfenix414 <greenfenix414@gmail.com>
2025-06-18 08:40:50 -0500

    reoganized files and started oop

--- main.py
+++ main.py
@@ -1,69 +1,74 @@
 import pygame
 
-pygame.init()   
+class game:
+    def __init__(self):
+        pygame.init()   
+        self.screen=pygame.display.set_mode((640,640))
+        pygame.display.set_caption('Richards adventures 1')
+        pygame.display.set_icon(pygame.image.load("data/richard.png"))
+        self.clock=pygame.time.Clock()
+        self.running=True
+        self.character_img=pygame.image.load('data/richard.png').convert()
+        self.sound=pygame.mixer.music.load("data/donuts.wav")
+        pygame.mixer.music.play(-1)
+        self.delta=0.1
+        speed =140
+        self.current_speed =speed
+        self.x=0
+        self.y=0
+        self.move_keys_down=0
+        self.move_right = False
+        self.move_down = False
+        self.move_left = False
+        self.move_up = False           
+            
+    def run(self):
 
-screen=pygame.display.set_mode((640,640))
-pygame.display.set_caption('Richards adventures 1')
-character_img=pygame.image.load('character.png').convert()
-running=True
-clock=pygame.time.Clock()
-sound=pygame.mixer.music.load("donuts.wav")
-pygame.mixer.music.play(-1)
-delta=0.1
-speed =140
-current_speed =speed
-x=0
-y=0
-move_keys_down=0
-move_right = False
-move_down = False
-move_left = False
-move_up = False
-while running:
-    screen.fill("white")
-    screen.blit(character_img,(x,y))
-    if move_keys_down>1:
-        current_speed=speed/2
-    else:
-        current_speed=speed   
-    if move_right:
-        x+=current_speed*delta
-    if move_down:
-        y+=current_speed*delta
-    if move_left:
-        x-=current_speed*delta
-    if move_up:
-        y-=current_speed*delta           
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            running=False
-        if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_d:
-                move_keys_down+=1
-                move_right=True
-            if event.key==pygame.K_s:
-                move_keys_down+=1
-                move_down=True     
-            if event.key==pygame.K_a:
-                move_keys_down+=1
-                move_left=True
-            if event.key==pygame.K_w:
-                move_keys_down+=1
-                move_up=True                         
-        if event.type==pygame.KEYUP:
-            if event.key==pygame.K_d:
-                move_keys_down-=1
-                move_right=False
-            if event.key==pygame.K_s:
-                move_keys_down-=1
-                move_down=False     
-            if event.key==pygame.K_a:
-                move_keys_down-=1
-                move_left=False
-            if event.key==pygame.K_w:
-                move_keys_down-=1
-                move_up=False                    
-    pygame.display.flip()
-    delta = clock.tick(60)/1000
-    delta=max(0.001, min(0.1,delta))
-pygame.quit()
+        while running:
+            screen.fill("white")
+            screen.blit(character_img,(x,y))
+            if move_keys_down>1:
+                current_speed=speed/2
+            else:
+                current_speed=speed   
+            if move_right:
+                x+=current_speed*delta
+            if move_down:
+                y+=current_speed*delta
+            if move_left:
+                x-=current_speed*delta
+            if move_up:
+                y-=current_speed*delta           
+            for event in pygame.event.get():
+                if event.type==pygame.QUIT:
+                    running=False
+                if event.type==pygame.KEYDOWN:
+                    if event.key==pygame.K_d:
+                        move_keys_down+=1
+                        move_right=True
+                    if event.key==pygame.K_s:
+                        move_keys_down+=1
+                        move_down=True     
+                    if event.key==pygame.K_a:
+                        move_keys_down+=1
+                        move_left=True
+                    if event.key==pygame.K_w:
+                        move_keys_down+=1
+                        move_up=True                         
+                if event.type==pygame.KEYUP:
+                    if event.key==pygame.K_d:
+                        move_keys_down-=1
+                        move_right=False
+                    if event.key==pygame.K_s:
+                        move_keys_down-=1
+                        move_down=False     
+                    if event.key==pygame.K_a:
+                        move_keys_down-=1
+                        move_left=False
+                    if event.key==pygame.K_w:
+                        move_keys_down-=1
+                        move_up=False                    
+            pygame.display.flip()
+            delta = clock.tick(60)/1000
+            delta=max(0.001, min(0.1,delta))
+        pygame.quit()
